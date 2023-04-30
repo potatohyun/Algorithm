e560fdfa74c7930d7bc24520087cd20ce6eba420
potatohyun <110168477+potatohyun@users.noreply.github.com>
2023-04-30 16:45:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.83 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,17 +2,31 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        // String answer = "";
+        // Arrays.sort(participant);
+        // Arrays.sort(completion);
+        // for(int i = 0; i < participant.length; i++){
+        //     if(i == completion.length){
+        //         answer += participant[i];
+        //     }
+        //     else if(!participant[i].equals(completion[i])){
+        //         answer += participant[i];
+        //         break;
+        //     }
+        // }
+        
+        //다른사람 풀이
         String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        for(int i = 0; i < participant.length; i++){
-            // System.out.println("i : "+i+"그리고 completion.length : "+completion.length);
-            if(i == completion.length){
-                answer += participant[i];
-            }
-            else if(!participant[i].equals(completion[i])){
-                answer += participant[i];
-                break;
+        HashMap<String, Integer> hm = new HashMap<>();
+        //key에 선수이름, value에 그 선수이름을 가진 사람의 수(동명이인때문에)
+        //getOrDefault(A,B) : 있으면 A반환, 없으면B반환
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        // System.out.println(hm);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
             }
         }
         
