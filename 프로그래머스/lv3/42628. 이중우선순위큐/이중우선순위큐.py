691fd7ada63b5b6b72e3533f3e1d8025dbf27e72
KIM JIHYUN <110168477+potatohyun@users.noreply.github.com>
2023-09-07 05:54:26 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -9,19 +9,21 @@ def solution(operations):
         
         if spell == 'I':
             heapq.heappush(que,int(num))
+            # print(que)
         elif spell == 'D' and que:    # 빈 큐라면 무시
             if num =='1':
+                que.sort()
                 que.pop()
             else:
                 heapq.heappop(que)
             
     if que:
+        # que.sort()
         # answer.append(que[-1])
         # answer.append(que[0])
-        answer.append(max(que))
-        answer.append(min(que))
+        answer = [max(que),min(que)]
     else:
         answer = [0,0]
-    # print(answer)
+    tmptmp =[1,1]
     
     return answer
