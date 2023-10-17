7141405884b6cb5e7649e84cdeae27dff26f4050
KIM JIHYUN <110168477+potatohyun@users.noreply.github.com>
2023-10-18 00:06:24 +0900

    [level unrated] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if(num1 == num2){
+            return 1;
+        }else{
+            return -1;
+        }
+        // return answer;
+        // Unreachable Statement 오류란 뜻 그대로 "접근 할 수 없는 구문" 이라는 뜻이며
+        // * Unreachable Statement 오류가 난 경우 당황할 필요는 없으며 자신이 작성한 코드가             논리적으로 맞는지 문법적으로 맞는지 한번 더 확인!
+    }
+}
