b360e95cf55fd844cd5c2a4028bea7202827ac00
KIM JIHYUN <110168477+potatohyun@users.noreply.github.com>
2024-03-14 00:24:26 +0900

    [level 2] Title: 숫자 변환하기, Time: 11.66 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    private static final int MAX = Integer.MAX_VALUE;
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        int[] dp = new int[y+1];
+
+        for(int i=x;i<=y;i++){
+            if(i!=x && dp[i]==0){
+                dp[i]=-1;
+            }
+            else{
+                if(i*3<=y) {
+                    dp[i*3] = dp[i*3]==0 ? dp[i]+1 : Math.min(dp[i*3], dp[i]+1);
+                }
+                if(i*2<=y) {
+                    dp[i*2] = dp[i*2]==0 ? dp[i]+1 : Math.min(dp[i*2], dp[i]+1);
+               }
+                if(i+n<=y) {
+                    dp[i+n] = dp[i+n]==0 ? dp[i]+1 : Math.min(dp[i+n], dp[i]+1);
+                }
+            }
+
+        }
+        return dp[y];
+    }
+}
