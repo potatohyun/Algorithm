299ab4d2999633ae98de64cffdcf6094b9a55aea
KIM JIHYUN <110168477+potatohyun@users.noreply.github.com>
2024-03-15 21:37:40 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.46 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -4,9 +4,10 @@ class Solution {
         int[] dp = new int[n+1];
         dp[1] = 1;
         dp[2] = 2;
-        for(int i = 3; i <= n; i++){
-            dp[i] = (dp[i-1] + dp[i-2])%1000000007;
+        for(int i=3; i<=n; i++){
+            dp[i]=(dp[i-1]+dp[i-2])% 1000000007;
         }
-        return dp[n];
+        answer = dp[n];
+        return answer;
     }
 }
