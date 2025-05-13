9b428df25ffd8fccea711d3c698524267436c805
KIM JIHYUN <110168477+potatohyun@users.noreply.github.com>
2025-05-13 23:59:05 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,35 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
         Map<String, Integer> map = new HashMap<>();
-        
-        for(String[] c: clothes){
-            if(!map.containsKey(c[1])){
-                map.put(c[1],1);
-            }else{
-                map.put(c[1],map.get(c[1])+1);
-            }
+        for(String[] clothe: clothes){
+            map.put(clothe[1], map.getOrDefault(clothe[1],0)+1);
         }
-        for (Integer v : map.values()) {
-          // System.out.println(v);
-            answer = answer * (v+1);
+        int answer = 1;
+        for(String k :map.keySet()){
+            answer*=map.get(k)+1;
         }
         answer--;
-        
-        // map.forEach((k,v) -> System.out.println(String.format("Key (name) is: %s, Value (age) is : %s", k, v)));        
-        
-//         //entrySet활용
-//         for (Entry<Integer, String> entry : map.entrySet()) {
-//             System.out.println("Key:"+entry.getKey()+"Value:"+entry.getValue());
-//         }
-
-//         //KeySet() 활용
-//         for(Integer i : map.keySet()){ //저장된 key값 확인
-//             System.out.println("[Key]:" + i + " [Value]:" + map.get(i));
-//         }
-
-        
         return answer;
     }
 }
